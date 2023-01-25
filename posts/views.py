c8a10efb4bcd997db819dc9af21438d7929a9de4
Gaurav <gauravsomani52750@gmail.com>
2023-01-25 18:34:04 +0530

    drf permissions => view lvl, project pvl

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,5 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.permissions import IsAuthenticated
 
 from .models import Post
 from .serializers import PostSerializer
@@ -6,10 +7,15 @@ from .serializers import PostSerializer
 
 # *Create your views here.
 class PostList(ListCreateAPIView):
+    # todo Add permission at view level
+    # permission_classes = [IsAuthenticated]
+
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
 
 class PostDetail(RetrieveUpdateDestroyAPIView):
+    # permission_classes = [IsAuthenticated]
+
     queryset = Post.objects.all()
     serializer_class = PostSerializer
